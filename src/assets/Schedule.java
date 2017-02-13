fcb03552919d3db138bf7b1c2d74a2b4a462a66f
Michel de Nijs <micheldenijs@mail.com>
2017-02-13 13:48:04 +0100

    Onnodige bestanden verwijderd Schedule krijgt een nieuwe taak Oude Schedule hernoemd naar Performance

--- src/assets/Schedule.java
+++ src/assets/Schedule.java
@@ -1,54 +1,24 @@
 package assets;
 
-/**
- * Created by Bob, Bram en Michael on 6-2-2017.
- */
+import java.util.ArrayList;
 
+/**
+ * Created by Michel on 13-2-2017.
+ */
 public class Schedule {
-    private Artist artist;
-    private Stage stage;
-    private double begin;
-    private double length;
+    private ArrayList<Performance> performances;
 
     public Schedule() {
-        artist = new Artist();
-        stage = new Stage();
-        begin = 2.0;
-        length = 3.0;
+        performances = new ArrayList<>();
     }
 
-    public Schedule(Artist artist, Stage stage, int begin, int length) {
-        this.artist = artist;
-        this.stage = stage;
-        this.begin = begin;
-        this.length = length;
+    public void addPerformance(Performance performance)
+    {
+        performances.add(performance);
     }
 
-    public double getBegin(){
-        return begin;
-    }
-
-    public Artist getAtrist(){
-        return artist;
-    }
-
-    public Stage getStage(){
-        return stage;
-    }
-
-    public double getLength(){
-        return length;
-    }
-
-    public double getEnd(){
-        double end;
-        end = begin + length;
-            if(end >= 24){
-                end -= 24;
-                return end;
-            }
-            else{
-                return end;
-            }
+    public Performance get(int index)
+    {
+        return performances.get(index);
     }
 }
--- src/assets/Toilet.java
+++
@@ -1,9 +0,0 @@
-package assets;
-
-/**
- * Created by Bobsk on 6-2-2017.
- */
-public class Toilet extends Facility {
-    private boolean available;
-    private java.util.ArrayList<Employee> employees;
-}
