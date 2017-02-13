5c67416ee7d99cdfea1cf767891b3e29992a6a28
Bram Stoof <bramstoof@live.com>
2017-02-13 10:33:40 +0100

    Renamed Artiest

--- src/assets/Artiest.java
+++ src/assets/Artiest.java
@@ -3,8 +3,28 @@ package assets;
 /**
  * Created by Bobsk on 6-2-2017.
  */
-public class Artist extends Stage {
+public class Artist{
     private String name;
     private String genre;
     private int members;
+
+public Artist(String name, String genre, int members){
+    this.name = name;
+    this.genre = genre;
+    this.members = members;
 }
+
+    public String getName(){
+        return name;
+    }
+
+    public String getGenre(){
+        return genre;
+    }
+
+    public int getMembers(){
+        return members;
+    }
+}
+
+
