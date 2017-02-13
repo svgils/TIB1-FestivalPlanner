6eb549266fdd2671dc4efaa4ef606cff6631e2ab
Bob <bwiel1@avans.nl>
2017-02-13 10:34:43 +0100

    Stage Methode toegevoegd & Klassen diagram

--- src/assets/Podia.java
+++ src/assets/Podia.java
@@ -3,7 +3,21 @@ package assets;
 /**
  * Created by Bobsk on 6-2-2017.
  */
-public class Stage extends Festival {
+public class Stage  {
     private String name;
     private String location;
+
+    public Stage(String name, String location){
+        this.name = name;
+        this.location = location;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public String getLocation(){
+        return location;
+    }
+
 }
