bf85f8876630cf5581b0f5c73286ce847925d63f
Bob <bwiel1@avans.nl>
2017-02-20 13:59:24 +0100

    +Added Stage gui

---
+++ src/gui/StageForm.java
@@ -0,0 +1,14 @@
+package gui;
+
+import javax.swing.*;
+
+/**
+ * Created by Bobsk on 20-2-2017.
+ */
+public class StageForm {
+    private JPanel panel1;
+    private JTextField textField1;
+    private JTextField textField2;
+    private JButton okButton;
+    private JButton cancelButton;
+}
