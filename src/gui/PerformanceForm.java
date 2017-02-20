2b2f4a8636a01aed55a2cfb1a46c02afdd1d95ea
Bob <bwiel1@avans.nl>
2017-02-20 10:20:39 +0100

    Added PerformanceForm

---
+++ src/gui/PerformanceForm.java
@@ -0,0 +1,16 @@
+package gui;
+
+import javax.swing.*;
+import java.awt.event.ContainerAdapter;
+import java.awt.event.ContainerEvent;
+
+/**
+ * Created by Bobsk on 20-2-2017.
+ */
+public class PerformanceForm extends JFrame {
+    private JTextField textField3;
+    private JTextField textField2;
+    private JTextField textField1;
+    private JButton okButton;
+    private JButton cancelButton;
+
